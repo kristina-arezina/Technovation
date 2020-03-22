27ad966eb79ac9263c469dedec69c3045c4ae229
Kristina Arezina <arezina.kristina23@gmail.com>
2020-03-22 09:09:22 -0400

    made images

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Logo from "./Components/Logo.js";
 import MissionStatment from "./Components/MissionStatment.js";
 import AppLink from "./Components/AppLink.js";
 import ImagesOutput from "./Components/ImagesOutput.js"
-
+import styled, { keyframes } from "styled-components";
+import SlideInLeft from "@bit/formidablelabs.react-animations.slide-in-left";
+const SlideInLeftAnimation = keyframes`${SlideInLeft}`;
+const SlideInLeftDiv = styled.div`
+  animation: 5s ${SlideInLeftAnimation};
+`;
 
 
 function App() {
   return (
     <div className="App">
-
+    <SlideInLeftDiv>
+      <ImagesOutput />
+    </SlideInLeftDiv>
       <TextInANest/>
       <Logo/>
       <MissionStatment/>
       <AppLink/>
-    <ImagesOutput/>
     </div>
   );
 }
--- src/Components/ImagesOutput.js
+++ src/Components/ImagesOutput.js
@@ -4,12 +4,26 @@ import MiddleImage from "./Images/MiddleImage.js";
 import TopImage from "./Images/TopImage.js";
 
 function ImagesOutput() {
-  const css = { margin: 5 }
+  const css = {
+    visibility: "hidden",
+      padding: 0,
+      paddingTop: "10px",
+      display: "block",
+      width: "25px",
+      height: "4px"}
+
+  const wraper = { display: "block",
+    position: "relative",
+    width: "25px",
+    top:"800px",
+  left:"1175px"}
   return (
-  <div>
+  <div style={wraper}>
+  <TopImage style={css}/>
+  <MiddleImage style={css}/>
       <Header style={css}/>
-      <MiddleImage style={css}/>
-      <TopImage style={css}/>
+
+
     </div>
   );
 }
